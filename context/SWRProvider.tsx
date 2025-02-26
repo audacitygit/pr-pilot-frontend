394cef36ddabbead05487663e1ceba4232c3c367
audacitygit <audacitygit@gmail.com>
2025-02-26 10:05:18 -0800

    updated swr provider

--- context/SWRProvider.tsx
+++ context/SWRProvider.tsx
@@ -1,17 +1,19 @@
-"use client"; // Mark this file as a Client Component
+"use client"
 
 import { SWRConfig } from "swr";
 import axios from "axios";
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+const fetcher = (url: string) =>
+    axios.get(url, { withCredentials: true }) // ✅ Ensures cookies are included
+        .then((res) => res.data);
 
 export default function SWRProvider({ children }: { children: React.ReactNode }) {
     return (
         <SWRConfig
             value={{
                 fetcher,
-                dedupingInterval: 5000, // Avoid duplicate requests in a short time
-                revalidateOnFocus: false, // Disable revalidation when window gains focus
+                dedupingInterval: 5000,
+                revalidateOnFocus: false,
             }}
         >
             {children}

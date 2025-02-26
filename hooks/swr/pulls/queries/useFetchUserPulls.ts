1b8d89077ecd8ee2d1293b7b4c24329ba2be8dd5
audacitygit <audacitygit@gmail.com>
2025-02-26 11:00:49 -0800

    fixes

--- hooks/swr/pulls/queries/useFetchUserPulls.ts
+++ hooks/swr/pulls/queries/useFetchUserPulls.ts
@@ -4,9 +4,9 @@ import useSWR from "swr";
 
 export default function useFetchUserPulls() {
     const { data, error } = useSWR("/github/pulls/user/repos/:id", fetchUserPullRequests);
-
+    console.log("DATA IN HOOK", data)
     return {
-        pulls: data?.pullRequests || [],
+        pulls: data || [],
         loading: !data && !error,
         error,
     };
